edbeb9b48a21dae2c2c581b7d8693f698d367828
Fatih <88527389+faithandlogic@users.noreply.github.com>
2023-02-16 19:49:13 -0800

    palindrome exercise with stacks and queues

---
+++ Week 7/palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String myString = input.nextLine();
+        System.out.println(isPalindrome(myString));
+        input.close();
+    }
+
+    public static boolean isPalindrome(String s) {
+        LinkedStack<Character> stack = new LinkedStack<>();
+        LinkedQueue<Character> queue = new LinkedQueue<>();
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+            queue.enqueue(s.charAt(i));
+        }
+        while (!stack.isEmpty()) {
+            if (stack.pop() != queue.dequeue())
+                return false;
+        }
+        return true;
+    }
+}
