b619cc53ba5d16e0056576c459c9aebce94a5ddb
Fatih <88527389+faithandlogic@users.noreply.github.com>
2023-02-25 22:21:37 -0800

    Facebook Data Structure

--- midterm/social_network.java
+++ midterm/social_network.java
@@ -32,12 +32,17 @@ public class social_network {
             leavingProfilesFriends[i].removeFriend(leavingProfile);
         }
         // remove the profile from the social network
+        System.out.println(name + " has left from the social network.");
         socialNetwork.remove(name);
     }
 
     // search profile
     public Profile searchProfile(String name) {
-        return socialNetwork.getValue(name);
+        if (socialNetwork.contains(name)) {
+            return socialNetwork.getValue(name);
+        } else {
+            return socialNetwork.getValue("Profile Not Found");
+        }
     }
 
     public class Profile {
@@ -100,32 +105,106 @@ public class social_network {
             return friends;
         }
 
+        // print friends list
+        public void printFriends() {
+            System.out.print(this.getName() + "'s friends are: ");
+            for (int i = 0; i < friends.getLength(); i++) {
+                System.out.print(friends.getEntry(i + 1).getName() + " ");
+            }
+            System.out.println();
+        }
+
     } // end Profile class
 
-    // test the social network, with names Fatih, Selma, Ismail, and Mehmet. Name the social network facebook.
+    // test
     public static void main(String[] args) {
         social_network facebook = new social_network();
+
+        // join network
+
+        System.out.println("Test Joining network...");
+
         facebook.joinNetwork("Fatih");
         facebook.joinNetwork("Selma");
         facebook.joinNetwork("Ismail");
         facebook.joinNetwork("Mehmet");
+        facebook.joinNetwork("Ali");
+        facebook.joinNetwork("Veli");
+        facebook.joinNetwork("Ayse");
+        facebook.joinNetwork("Fatma");
+
+        // missing profile dummy profile
+        facebook.joinNetwork("Profile Not Found");
+
+        System.out.println("Test Search network...");
+
+        // search profile
+        System.out.println(facebook.searchProfile("Fatih").getName());
+        System.out.println(facebook.searchProfile("Selma").getName());
+        System.out.println(facebook.searchProfile("Ismail").getName());
+        System.out.println(facebook.searchProfile("Mehmet").getName());
+
+        System.out.println("Test Adding friends...");
+
+        // add friend
         facebook.searchProfile("Fatih").addFriend(facebook.searchProfile("Selma"));
         facebook.searchProfile("Fatih").addFriend(facebook.searchProfile("Ismail"));
         facebook.searchProfile("Fatih").addFriend(facebook.searchProfile("Mehmet"));
+
         facebook.searchProfile("Selma").addFriend(facebook.searchProfile("Fatih"));
         facebook.searchProfile("Selma").addFriend(facebook.searchProfile("Ismail"));
-        facebook.searchProfile("Selma").addFriend(facebook.searchProfile("Mehmet"));
+
         facebook.searchProfile("Ismail").addFriend(facebook.searchProfile("Fatih"));
         facebook.searchProfile("Ismail").addFriend(facebook.searchProfile("Selma"));
         facebook.searchProfile("Ismail").addFriend(facebook.searchProfile("Mehmet"));
-        facebook.searchProfile("Mehmet").addFriend(facebook.searchProfile("Fatih"));
-        facebook.searchProfile("Mehmet").addFriend(facebook.searchProfile("Selma"));
-        facebook.searchProfile("Mehmet").addFriend(facebook.searchProfile("Ismail"));
+        facebook.searchProfile("Ismail").addFriend(facebook.searchProfile("Ali"));
+        facebook.searchProfile("Ismail").addFriend(facebook.searchProfile("Veli"));
+
+        System.out.println("Test Removing friends...");
+
+        // remove friend
+
+        facebook.searchProfile("Fatih").removeFriend(facebook.searchProfile("Selma"));
+
+        System.out.println("Test Setting status...");
+
+        // set status
+
+        facebook.searchProfile("Fatih").setStatus("I am Busy");
+        facebook.searchProfile("Selma").setStatus("Online");
+        facebook.searchProfile("Ismail").setStatus("Online");
+        facebook.searchProfile("Mehmet").setStatus("Offline");
+
+        System.out.println("Test leaving network...");
+
+        // leave network
+
         facebook.leaveNetwork("Fatih");
-        System.out.println(Arrays.toString(facebook.searchProfile("Selma").getFriends().toArray()));
-        System.out.println(Arrays.toString(facebook.searchProfile("Ismail").getFriends().toArray()));
-        System.out.println(Arrays.toString(facebook.searchProfile("Mehmet").getFriends().toArray()));
+        facebook.leaveNetwork("Selma");
+
+        System.out.println("Test Search network after leaving...");
+
+        // search profile
+
+        System.out.println(facebook.searchProfile("Fatih").getName());
+        System.out.println(facebook.searchProfile("Selma").getName());
+        System.out.println(facebook.searchProfile("Ismail").getName());
+
+        System.out.println("Test Printing friends...");
+
+        // print friends
+
+        facebook.searchProfile("Ismail").printFriends();
+
+        System.out.println("Test Printing status...");
+        // print status
+
+        System.out.println(facebook.searchProfile("Ismail").getStatus());
+        System.out.println(facebook.searchProfile("Mehmet").getStatus());
+        System.out.println(facebook.searchProfile("Ali").getStatus());
+        System.out.println(facebook.searchProfile("Veli").getStatus());
+        System.out.println(facebook.searchProfile("Ayse").getStatus());
+
     }
 
 } // end social_network class
-
